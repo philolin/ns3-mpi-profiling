d20cb2ff5e914f3841a7717f60bf9c154425a549
philolin <philolin@node0.philolin.utcs395t-pg0.utah.cloudlab.us>
2023-10-12 15:54:34 -0600

    update startup script

--- startup.sh
+++ startup.sh
@@ -2,3 +2,8 @@
 
 sudo apt-get update
 sudo apt-get -y install cmake
+
+cd ns-allinone-3.40/ns-3.40
+./ns3 clean
+./ns3 configure --build-profile=debug --enable-examples --enable-tests
+./ns3 build -j8
